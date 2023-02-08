46affb288ee78b62c7b399932894c13dd9972d57
Callum Anning <Callum5042@gmail.com>
2023-02-08 19:41:26 +0000

    Working copy

--- Chip8-Emulator/Chip8.hpp
+++ Chip8-Emulator/Chip8.hpp
@@ -144,9 +144,9 @@ private:
 	uint16_t opcode{};
 
 	std::default_random_engine randGen;
-	std::uniform_int_distribution<int> randByte;
+	std::uniform_int_distribution<uint16_t> randByte;
 
-	typedef void (Chip8::* Chip8Func)();
+	typedef void (Chip8::*Chip8Func)();
 	Chip8Func table[0xF + 1];
 	Chip8Func table0[0xE + 1];
 	Chip8Func table8[0xE + 1];
--- Chip8-Emulator/Main.cpp
+++ Chip8-Emulator/Main.cpp
@@ -1,169 +1,47 @@
-#include <iostream>
-#include <Windows.h>
-#include <fstream>
+#include "Chip8.hpp"
+#include "Platform.hpp"
 #include <chrono>
-#include <thread>
-#include "Chip8.h"
-#include <SDL.h>
-
-//Chip8 emulator;
-
-//LRESULT CALLBACK MainWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
-//{
-//	switch (msg)
-//	{
-//		case WM_DESTROY:
-//			PostQuitMessage(0);
-//			return 0;
-//
-//		case WM_PAINT:
-//		{
-//			PAINTSTRUCT ps;
-//			HDC hdc = BeginPaint(hwnd, &ps);
-//
-//			// All painting occurs here, between BeginPaint and EndPaint.
-//			FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-//
-//
-//			int videoPitch = sizeof(emulator.video[0]) * VIDEO_WIDTH;
-//
-//			for (int i = 0; i < 64; ++i)
-//			{
-//				for (int j = 0; j < 32; ++j)
-//				{
-//					int xpos = emulator.video[i];
-//					int ypos = emulator.video[i];
-//					SetPixel(hdc, xpos, ypos, RGB(0, 0, 0));
-//				}
-//			}
-//
-//			EndPaint(hwnd, &ps);
-//			return 0;
-//		}
-//	}
-//
-//	return DefWindowProc(hwnd, msg, wParam, lParam);
-//}
-//
-//int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
-//{
-//	// Setup window class
-//	WNDCLASS wc = {};
-//	wc.style = CS_VREDRAW | CS_HREDRAW;
-//	wc.lpfnWndProc = MainWndProc;
-//	wc.hInstance = hInstance;
-//	wc.hIcon = LoadIcon(0, IDI_APPLICATION);
-//	wc.hCursor = LoadCursor(0, IDC_ARROW);
-//	wc.lpszClassName = L"Chip8-Emulator";
-//
-//	if (!RegisterClass(&wc))
-//	{
-//		MessageBox(NULL, L"Error", L"RegisterClass Failed", MB_OK | MB_ICONERROR);
-//		return -1;
-//	}
-//
-//	// Create window
-//	HWND m_Hwnd = CreateWindow(wc.lpszClassName, L"Chip 8 Emulator", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL);
-//	if (m_Hwnd == NULL)
-//	{
-//		MessageBox(NULL, L"Error", L"CreateWindow Failed", MB_OK | MB_ICONERROR);
-//		return -1;
-//	}
-//
-//	ShowWindow(m_Hwnd, SW_SHOWNORMAL);
-//
-//	// Emulator
-//	emulator.LoadROM("test_opcode.ch8");
-//
-//	int videoPitch = sizeof(emulator.video[0]) * VIDEO_WIDTH;
-//
-//	// Loop
-//	MSG msg = {};
-//	bool m_IsRunning = true;
-//	while (m_IsRunning)
-//	{
-//		while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
-//		{
-//			if (msg.message == WM_QUIT)
-//			{
-//				m_IsRunning = false;
-//			}
-//
-//			TranslateMessage(&msg);
-//			DispatchMessageW(&msg);
-//		}
-//
-//		emulator.Cycle();
-//
-//		RedrawWindow(m_Hwnd, NULL, NULL, RDW_INVALIDATE);
-//
-//		using namespace std::chrono_literals;
-//		std::this_thread::sleep_for(16ms);
-//	}
-//
-//	DestroyWindow(m_Hwnd);
-//	return static_cast<int>(msg.wParam);
-//}
+#include <iostream>
+#include <string>
 
 int main(int argc, char** argv)
 {
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+	/*if (argc != 4)
+	{
+		std::cerr << "Usage: " << argv[0] << " <Scale> <Delay> <ROM>\n";
+		std::exit(EXIT_FAILURE);
+	}*/
 
-    // Initialise SDL subsystems
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0)
-    {
-        auto error = SDL_GetError();
-        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", error, nullptr);
-        return false;
-    }
+	int videoScale = 10;//std::stoi(argv[1]);
+	int cycleDelay = 60; //std::stoi(argv[2]);
+	char const* romFilename = "test_opcode.ch8"; //argv[3];
 
-    // Create SDL Window
-    auto window_flags = SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL;
-    SDL_Window* window = SDL_CreateWindow("DirectX - Perspective Camera", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, window_flags);
-    if (window == nullptr)
-    {
-        std::string error = "SDL_CreateWindow failed: ";
-        error += SDL_GetError();
-        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", error.c_str(), nullptr);
-        return false;
-    }
+	Platform platform("CHIP-8 Emulator", VIDEO_WIDTH * videoScale, VIDEO_HEIGHT * videoScale, VIDEO_WIDTH, VIDEO_HEIGHT);
 
-    // Renderer
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (renderer == nullptr)
-    {
-        std::string error = "SDL_CreateRenderer failed: ";
-        error += SDL_GetError();
-        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", error.c_str(), nullptr);
-        return false;
-    }
+	Chip8 chip8;
+	chip8.LoadROM(romFilename);
 
-    // Emulator
-    Chip8 emulator;
-    emulator.LoadROM("test_opcode.ch8");
-    int videoPitch = sizeof(emulator.video[0]) * VIDEO_WIDTH;
+	int videoPitch = sizeof(chip8.video[0]) * VIDEO_WIDTH;
 
-    SDL_Texture* texture{};
+	auto lastCycleTime = std::chrono::high_resolution_clock::now();
+	bool quit = false;
 
+	while (!quit)
+	{
+		quit = platform.ProcessInput(chip8.keypad);
 
-    SDL_Event e = {};
-    while (e.type != SDL_QUIT)
-    {
-        if (SDL_PollEvent(&e))
-        {
+		auto currentTime = std::chrono::high_resolution_clock::now();
+		float dt = std::chrono::duration<float, std::chrono::milliseconds::period>(currentTime - lastCycleTime).count();
 
-        }
+		if (dt > cycleDelay)
+		{
+			lastCycleTime = currentTime;
 
-        emulator.Cycle();
+			chip8.Cycle();
 
-        SDL_UpdateTexture(texture, nullptr, emulator.video, videoPitch);
-        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-        SDL_RenderClear(renderer);
-        SDL_RenderCopy(renderer, texture, nullptr, nullptr);
-        SDL_RenderPresent(renderer);
-    }
+			platform.Update(chip8.video, videoPitch);
+		}
+	}
 
 	return 0;
 }
